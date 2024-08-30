42695211abe2181ad0106ad412b603b63963d47e
Yi Lin <qinsoon@gmail.com>
2024-08-30 01:40:54 +0000

    expose some globally rooted symbols

--- src/staticdata_utils.c
+++ src/staticdata_utils.c
@@ -83,7 +83,7 @@ static uint64_t jl_worklist_key(jl_array_t *worklist) JL_NOTSAFEPOINT
     return 0;
 }
 
-static jl_array_t *newly_inferred JL_GLOBALLY_ROOTED /*FIXME*/;
+jl_array_t *newly_inferred JL_GLOBALLY_ROOTED /*FIXME*/;
 // Mutex for newly_inferred
 static jl_mutex_t newly_inferred_mutex;
 
