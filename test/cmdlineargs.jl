0b5f9af31112d6ae472465fc5dfdb51e5c8df414
Yi Lin <qinsoon@gmail.com>
2026-01-22 21:16:53 +0000

    Minor

--- test/cmdlineargs.jl
+++ test/cmdlineargs.jl
@@ -1365,7 +1365,7 @@ end
 @test readchomp(`$(Base.julia_cmd()) -e 'module Hello; export main; (@main)(args) = println("hello"); end; import .Hello'`) == ""
 
 # test --bug-report=rr
-if Sys.islinux() && Sys.ARCH in (:i686, :x86_64) && # rr is only available on these platforms
+if Sys.islinux() && Sys.ARCH in (:i686, :x86_64) # rr is only available on these platforms
     mktempdir() do temp_trace_dir
         cmd = setenv(`$(Base.julia_cmd()) --bug-report=rr-local -e 'exit()'`,
                      "JULIA_RR_RECORD_ARGS" => "-n --nested=ignore",
